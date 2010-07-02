72b8f4ce1ef26a5da4cd3ce22a32568377d459dc
Ralph Schaer <ralphschaer@gmail.com>
2010-07-02 07:54:07 +0000

    reformat source code

--- extdirectspring-demo/src/main/java/ch/ralscha/extdirectspring/demo/Poll.java
+++ extdirectspring-demo/src/main/java/ch/ralscha/extdirectspring/demo/Poll.java
@@ -35,8 +35,10 @@ public class Poll {
   }
 
   @ExtDirectMethod(value = ExtDirectMethodType.POLL, event = "pollWithParams", group = "example")
-  public String pollingWithParams(@RequestParam(value = "no") int no, @RequestParam(value = "name") String name,
-      @RequestParam(value = "dummy", defaultValue = "CH") String dummy, HttpServletRequest request) {
+  public String pollingWithParams(@RequestParam(value = "no") int no, 
+      @RequestParam(value = "name") String name,
+      @RequestParam(value = "dummy", defaultValue = "CH") String dummy, 
+      HttpServletRequest request) {
     return request.getRequestURI() + ":  POST PARAMETERS: no=" + no + ", name=" + name + ", dummy=" + dummy;
   }
 }
