cb1001f985698f8218c260e4ddd73cff0a540c40
Ralph Schaer <ralphschaer@gmail.com>
2011-03-20 13:29:57 +0000

    updated license header with current year

--- extdirectspring-demo/src/main/java/ch/ralscha/extdirectspring/demo/pivot/SalesAction.java
+++ extdirectspring-demo/src/main/java/ch/ralscha/extdirectspring/demo/pivot/SalesAction.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010 Ralph Schaer <ralphschaer@gmail.com>
+ * Copyright 2010-2011 Ralph Schaer <ralphschaer@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ch.ralscha.extdirectspring.demo.pivot;
 
 import java.util.List;
