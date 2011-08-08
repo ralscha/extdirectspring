bc6962b11d28fab5e2e255a051c14a885d131974
Ralph Schaer <ralphschaer@gmail.com>
2011-08-08 11:32:36 +0000

    added statehandling

--- starter-app/src/main/webapp/app.js
+++ starter-app/src/main/webapp/app.js
@@ -7,22 +7,27 @@ Ext.application({
 	autoCreateViewport: true,
 	launch: function() {
 
+		if (this.hasLocalstorage()) {
+			Ext.state.Manager.setProvider(Ext.create('Ext.state.LocalStorageProvider'));
+		} else {
+			Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
+		}
+
 		Ext.direct.Manager.on('event', function(e) {
-			//todo: need a better method to handle session timeouts
+			// todo: need a better method to handle session timeouts
 			if (e.code && e.code === 'parse') {
 				window.location.reload();
 			}
 		});
-		
-		Ext.direct.Manager.on('exception', function(e) {	
+
+		Ext.direct.Manager.on('exception', function(e) {
 			if (e.message === 'accessdenied') {
 				Starter.component.Notification.error(i18n.error, i18n.error_accessdenied);
 			} else {
 				Starter.component.Notification.error(i18n.error, e.message);
 			}
-		});		
-		
-		
+		});
+
 		Ext.apply(Ext.form.field.VTypes, {
 			password: function(val, field) {
 				if (field.initialPassField) {
@@ -35,5 +40,12 @@ Ext.application({
 			passwordText: i18n.user_passworddonotmatch
 		});
 
+	},
+	hasLocalstorage: function() {
+		try {
+			return !!localStorage.getItem;
+		} catch (e) {
+			return false;
+		}
 	}
 });
