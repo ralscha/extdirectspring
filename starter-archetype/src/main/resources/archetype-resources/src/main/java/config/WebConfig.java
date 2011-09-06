4b2982463cad23433aa01166237d6d8fa3b7e50e
Ralph Schaer <ralphschaer@gmail.com>
2011-09-06 12:45:32 +0000

    updated archetype

--- starter-archetype/src/main/resources/archetype-resources/src/main/java/config/WebConfig.java
+++ starter-archetype/src/main/resources/archetype-resources/src/main/java/config/WebConfig.java
@@ -4,7 +4,6 @@
 package ${package}.config;
 
 import java.util.Locale;
-import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -20,8 +19,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import ro.isdc.wro.config.jmx.ConfigConstants;
-import ro.isdc.wro.http.ConfigurableWroFilter;
 import ${package}.web.AppLocaleResolver;
 
 import com.google.common.collect.ImmutableMap;
@@ -80,38 +77,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		//messageSource.setCacheSeconds(60);
 
 		return messageSource;
-	}
-
-	@Bean
-	public ConfigurableWroFilter wroFilter() {
-		ConfigurableWroFilter filter = new ConfigurableWroFilter();
-		Properties properties = new Properties();
-
-		//If true, it is DEVELOPMENT mode, by default this value is true
-		properties.setProperty(ConfigConstants.debug.name(), "false");
-
-		// Default is true
-		properties.setProperty(ConfigConstants.gzipResources.name(), "true");
-		properties.setProperty(ConfigConstants.jmxEnabled.name(), "false");
-
-		// MBean name to be used if JMX is enabled
-		properties.setProperty(ConfigConstants.mbeanName.name(), "wro");
-
-		// Default is 0
-		properties.setProperty(ConfigConstants.cacheUpdatePeriod.name(), "0");
-
-		// Default is 0
-		properties.setProperty(ConfigConstants.modelUpdatePeriod.name(), "0");
-
-		// Default is false.
-		properties.setProperty(ConfigConstants.disableCache.name(), "false");
-
-		// Default is UTF-8
-		properties.setProperty(ConfigConstants.encoding.name(), "UTF-8");
-
-		filter.setProperties(properties);
-
-		return filter;
 	}
 
 }
