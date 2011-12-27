aa1a7b99137d1cfb8ef3cd8c3cba78e9006426cd
Ralph Schaer <ralphschaer@gmail.com>
2011-12-27 14:49:04 +0000

    updated jetty, guava, apache-poi

--- starter-app/src/main/java/ch/ralscha/starter/entity/QRole.java
+++ starter-app/src/main/java/ch/ralscha/starter/entity/QRole.java
@@ -1,14 +1,12 @@
 package ch.ralscha.starter.entity;
 
-import static com.mysema.query.types.PathMetadataFactory.forVariable;
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.*;
+import com.mysema.query.types.path.*;
 
 import javax.annotation.Generated;
 
-import com.mysema.query.types.Path;
-import com.mysema.query.types.PathMetadata;
-import com.mysema.query.types.path.EntityPathBase;
-import com.mysema.query.types.path.NumberPath;
-import com.mysema.query.types.path.StringPath;
 
 /**
  * QRole is a Querydsl query type for Role
@@ -16,27 +14,27 @@ import com.mysema.query.types.path.StringPath;
 @Generated("com.mysema.query.codegen.EntitySerializer")
 public class QRole extends EntityPathBase<Role> {
 
-	private static final long serialVersionUID = 1856413137;
+    private static final long serialVersionUID = 1856413137;
 
-	public static final QRole role = new QRole("role");
+    public static final QRole role = new QRole("role");
 
-	public final org.springframework.data.jpa.domain.QAbstractPersistable _super = new org.springframework.data.jpa.domain.QAbstractPersistable(
-			this);
+    public final org.springframework.data.jpa.domain.QAbstractPersistable _super = new org.springframework.data.jpa.domain.QAbstractPersistable(this);
 
-	public final NumberPath<Long> id = createNumber("id", Long.class);
+    public final NumberPath<Long> id = createNumber("id", Long.class);
 
-	public final StringPath name = createString("name");
+    public final StringPath name = createString("name");
 
-	public QRole(String variable) {
-		super(Role.class, forVariable(variable));
-	}
+    public QRole(String variable) {
+        super(Role.class, forVariable(variable));
+    }
 
-	public QRole(Path<? extends Role> entity) {
-		super(entity.getType(), entity.getMetadata());
-	}
+    public QRole(Path<? extends Role> entity) {
+        super(entity.getType(), entity.getMetadata());
+    }
 
-	public QRole(PathMetadata<?> metadata) {
-		super(Role.class, metadata);
-	}
+    public QRole(PathMetadata<?> metadata) {
+        super(Role.class, metadata);
+    }
 
 }
+
