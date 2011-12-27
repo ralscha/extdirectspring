1c75caf4655066cfbea4234c961839248a462303
Ralph Schaer <ralphschaer@gmail.com>
2011-12-27 15:13:02 +0000

    reorganized files into subfolders. addes demos for extjs4.1

--- extdirectspring-demo/src/main/java/ch/ralscha/extdirectspring/demo/upload/UploadController.java
+++ extdirectspring-demo/src/main/java/ch/ralscha/extdirectspring/demo/upload/UploadController.java
@@ -22,11 +22,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import ch.ralscha.extdirectspring.annotation.ExtDirectMethod;
