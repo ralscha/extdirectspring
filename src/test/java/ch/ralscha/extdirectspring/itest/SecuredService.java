cd394cf3e94a1a6bc8775a085ced0f4042af0fcd
Ralph Schaer <ralphschaer@gmail.com>
2012-04-26 18:17:08 +0000

    added a unit test for issue 30. now on to the bug hunting...

---
+++ src/test/java/ch/ralscha/extdirectspring/itest/SecuredService.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2010-2012 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.ralscha.extdirectspring.itest;
+
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ch.ralscha.extdirectspring.annotation.ExtDirectMethod;
+
+@Service
+public class SecuredService {
+
+	@ExtDirectMethod(group = "secured")
+	//@PreAuthorize("isAnonymous()")
+	public String setDateSecure(@DateTimeFormat(pattern = "dd/MM/yyyy") Date date) {
+		System.out.println(date);
+		return date.toString();
+	}
+	
+	@ExtDirectMethod(group = "secured")
+	public String setDate(@DateTimeFormat(pattern = "dd/MM/yyyy") Date date) {
+		System.out.println(date);
+		return date.toString();
+	}
+	
+	@ExtDirectMethod(group = "secured")
+	public String setDateWithRequestParam(@RequestParam(value="id", defaultValue="10") String id, @DateTimeFormat(pattern = "dd/MM/yyyy") Date date) {
+		System.out.println(id);
+		System.out.println(date);
+		return date.toString();
+	}
+	
+}
