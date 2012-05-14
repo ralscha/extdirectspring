68752809d939cffb8d1e34304749ec438d52c3bf
Ralph Schaer <ralphschaer@gmail.com>
2012-05-14 09:30:44 +0000

    moved JsonHandler back to his original package. no need to make migration more complex than needed

--- src/main/java/ch/ralscha/extdirectspring/util/JsonHandler.java
+++ src/main/java/ch/ralscha/extdirectspring/util/JsonHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ch.ralscha.extdirectspring.controller;
+package ch.ralscha.extdirectspring.util;
 
 import java.io.InputStream;
 
