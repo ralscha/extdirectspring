63a4223dee9bb9d5cd600fa048b703ec99107142
Ralph Schaer <ralphschaer@gmail.com>
2012-05-21 06:24:41 +0000

    update javadoc

--- src/main/java/ch/ralscha/extdirectspring/util/ParameterInfo.java
+++ src/main/java/ch/ralscha/extdirectspring/util/ParameterInfo.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ValueConstants;
 
 /**
- * Object holds information about a parameter as the name, type and the
+ * Object holds information about a parameter. i.e. the name, type and the
  * attributes of a RequestParam annotation
  * 
  * @author Ralph Schaer
