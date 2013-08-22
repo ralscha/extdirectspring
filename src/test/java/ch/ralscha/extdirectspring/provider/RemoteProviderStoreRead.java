74d9b9866b92c703b789ad2cd990380c847d3707
Ralph Schaer <ralphschaer@gmail.com>
2013-08-22 11:11:42 +0200

    Fix for the unit tests #97

--- src/test/java/ch/ralscha/extdirectspring/provider/RemoteProviderStoreRead.java
+++ src/test/java/ch/ralscha/extdirectspring/provider/RemoteProviderStoreRead.java
@@ -221,7 +221,7 @@ public class RemoteProviderStoreRead {
 
 		}
 
-		return new ExtDirectStoreResult<Row>().setTotal(Long.valueOf(totalSize)).setRecords(rows);
+		return new ExtDirectStoreResult<Row>().setTotal(Long.valueOf(totalSize)).setRecords(rows).setSuccess(true);
 
 	}
 
