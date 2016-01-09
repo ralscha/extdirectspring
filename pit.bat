34876e10d8ac008fda9920f0f7a0c27e8244ba9e
Ralph Schaer <ralphschaer@gmail.com>
2016-01-09 07:07:02 +0100

    Upgrade libraries Update year in license header

--- pit.bat
+++ pit.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright 2010-2014 Ralph Schaer <ralphschaer@gmail.com>
+@REM Copyright 2010-2016 Ralph Schaer <ralphschaer@gmail.com>
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
