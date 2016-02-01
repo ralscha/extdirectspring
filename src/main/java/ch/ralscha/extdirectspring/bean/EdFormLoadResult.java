606c65afa8b821227e931b059ccd41b5160d070e
Ralph Schaer <ralphschaer@gmail.com>
2016-02-01 18:37:12 +0100

    Create immutable for FormLoadResult

---
+++ src/main/java/ch/ralscha/extdirectspring/bean/EdFormLoadResult.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2010-2016 Ralph Schaer <ralphschaer@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.ralscha.extdirectspring.bean;
+
+import org.immutables.value.Value;
+import org.immutables.value.Value.Style.ImplementationVisibility;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+/**
+ * Represents the result of a FORM_LOAD method call.
+ */
+@JsonSerialize(as = ImmutableEdFormLoadResult.class)
+@Value.Style(visibility = ImplementationVisibility.PACKAGE)
+@Value.Immutable
+public abstract class EdFormLoadResult extends JsonViewHint {
+
+	@Value.Parameter
+	@Nullable
+	public abstract Object data();
+
+	@Value.Parameter
+	public abstract boolean success();
+
+	public static EdFormLoadResult success(Object data) {
+		return ImmutableEdFormLoadResult.of(data, true);
+	}
+
+	public static EdFormLoadResult fail(Object data) {
+		return ImmutableEdFormLoadResult.of(data, false);
+	}
+
+}
