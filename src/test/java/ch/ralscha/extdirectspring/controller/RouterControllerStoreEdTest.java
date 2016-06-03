773ff550cdf4d80f9381be412419d32a45ec3407
Ralph Schaer <ralphschaer@gmail.com>
2016-06-03 11:10:19 +0200

    Remove unused import

--- src/test/java/ch/ralscha/extdirectspring/controller/RouterControllerStoreEdTest.java
+++ src/test/java/ch/ralscha/extdirectspring/controller/RouterControllerStoreEdTest.java
@@ -44,7 +44,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ch.ralscha.extdirectspring.bean.EdStoreResult;
